9aaecfbbe957e254f6c7cbe5a1bc19e31135b901
Jan Hoskovec <jean.hoskovec@gmail.com>
2018-10-21 22:59:26 +0200

    nightly save

---
+++ FoodTruck.UI/ViewModels/ProductDetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using FoodTruck.Core.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTruck.UI.ViewModels
+{
+    public class ProductDetailsViewModel : BindableBase
+    {
+        private Produit _product;
+        public Produit Product { get { return _product; } set { SetProperty(ref _product, value); } }
+
+        private DelegateCommand _command;
+        public DelegateCommand Show { get { return _command; } }
+
+        public ProductDetailsViewModel()
+        {
+            _command = new DelegateCommand(DoShow);
+        }
+
+        private void DoShow()
+        {
+
+        }
+    }
+}
---
+++ FoodTruck.UI/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,30 @@
+﻿using FoodTruck.Core.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTruck.UI.ViewModels
+{
+    public class ShoppingCartViewModel : BindableBase
+    {
+        private DelegateCommand<Produit> _command;
+        public DelegateCommand<Produit> RemoveFromCart
+        {
+            get { return _command; }
+        }
+
+        public ShoppingCartViewModel()
+        {
+            _command = new DelegateCommand<Produit>(DoRemoveFromCart);
+        }
+
+        protected void DoRemoveFromCart(Produit p)
+        {
+            Session.Instance().Panier.Remove(p);
+        }
+    }
+}
